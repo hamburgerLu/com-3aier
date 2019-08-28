34059f883b1acbcc9972474c5aa912a8e2fbafd3
mfluhanbo <mfluhanbo@jd.com>
2019-08-28 19:34:28 +0800

    commit

--- src/doc/3aier.sql
+++ src/doc/3aier.sql
@@ -1,72 +1,64 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : 本地
-Source Server Version : 80017
-Source Host           : localhost:3306
-Source Database       : 3aier
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 50723
+ Source Host           : localhost:3306
+ Source Schema         : 3aier
 
-Target Server Type    : MYSQL
-Target Server Version : 80017
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50723
+ File Encoding         : 65001
 
-Date: 2019-08-28 00:46:14
+ Date: 28/08/2019 17:07:26
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for t_3aier_article
 -- ----------------------------
 DROP TABLE IF EXISTS `t_3aier_article`;
-CREATE TABLE `t_3aier_article` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `content` text COLLATE utf8mb4_bin,
-  `creater` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `create_time` datetime DEFAULT NULL,
-  `updater` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `update_time` datetime DEFAULT NULL,
-  `delete_flag` smallint(6) DEFAULT NULL,
-  `status` smallint(6) DEFAULT NULL,
-  `page_view` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
--- ----------------------------
--- Records of t_3aier_article
--- ----------------------------
+CREATE TABLE `t_3aier_article`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL,
+  `creater` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  `updater` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `update_time` datetime(0) NULL DEFAULT NULL,
+  `delete_flag` smallint(6) NULL DEFAULT NULL,
+  `status` smallint(6) NULL DEFAULT NULL,
+  `page_view` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_3aier_article_file
 -- ----------------------------
 DROP TABLE IF EXISTS `t_3aier_article_file`;
-CREATE TABLE `t_3aier_article_file` (
-  `id` int(11) NOT NULL,
-  `article_id` int(11) DEFAULT NULL,
-  `file_name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `file_address` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `delete_flag` smallint(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
--- ----------------------------
--- Records of t_3aier_article_file
--- ----------------------------
+CREATE TABLE `t_3aier_article_file`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NULL DEFAULT NULL,
+  `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `file_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `delete_flag` smallint(6) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_3aier_user
 -- ----------------------------
 DROP TABLE IF EXISTS `t_3aier_user`;
-CREATE TABLE `t_3aier_user` (
-  `id` int(11) NOT NULL,
-  `user_name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `pass_word` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `delete_flag` smallint(6) DEFAULT NULL,
-  `status` smallint(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+CREATE TABLE `t_3aier_user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `pass_word` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `delete_flag` smallint(6) NULL DEFAULT NULL,
+  `status` smallint(6) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of t_3aier_user
--- ----------------------------
+SET FOREIGN_KEY_CHECKS = 1;
